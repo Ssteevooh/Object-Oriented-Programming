c66bc83f554d28a7633c80d4a78dd16211e56825
Steve Hommy <steve96hommy@gmail.com>
2022-01-18 11:21:21 +0200

    Exercise1_3 Sorting list in ascending order

---
+++ exercise3.py
@@ -0,0 +1,27 @@
+# File name: exercise3.py
+# Author: Steve Hommy
+# Description: Listing items
+
+
+# Asking user for range of items that will be on list
+number_of_elements = int(input("Enter number of elements in list: "))
+
+# Creating lists
+number_list = []
+word_list = []
+
+# Appending intgeres and strings to the list
+for i in range(number_of_elements):
+    number = int(input("Enter number: "))
+    number_list.append(number)
+for i in range(number_of_elements):
+    word = input("Type anything: ")
+    word_list.append(word)
+
+# Sorting lists in ascending order
+number_list.sort()
+word_list.sort()
+
+# Prints out lists
+print(number_list)
+print(word_list)
