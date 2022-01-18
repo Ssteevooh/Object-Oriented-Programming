8103aee45a9c449afb14951c85d62cd010cfeb41
Steve Hommy <steve96hommy@gmail.com>
2022-01-18 11:02:40 +0200

    Exercise1_2 Appending items to list

---
+++ Exercise1/exercise2.py
@@ -0,0 +1,30 @@
+# File name: exercise1.py
+# Author: Steve Hommy
+# Description: Listing items
+
+# Importing random module
+import random
+
+# Asking user for range of items that will be on list
+number_of_elements = int(input("Enter number of elements in list: "))
+
+# Creating lists
+number_list = []
+word_list = []
+
+# Appending intgeres and strings to the list
+for i in range(number_of_elements):
+    number = int(input("Enter number: "))
+    number_list.append(number)
+for i in range(number_of_elements):
+    word = input("Type anything: ")
+    word_list.append(word)
+
+# Prints out lists
+print(number_list)
+print(word_list)
+
+# Filling the number list with randomly generated numbers
+for i in range(len(number_list)):
+    number_list[i] = random.randint(0,100)
+print(number_list)
