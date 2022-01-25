dc2ff0989ee2bf11cd9ead9d3cd419160e15f8ee
Steve Hommy <steve96hommy@gmail.com>
2022-01-25 10:10:41 +0200

    Exercise2_1 Pseudocode that prints out the grade

---
+++ Exercise2/exercise2.py
@@ -0,0 +1,20 @@
+# File name: exercise1.py
+# Author: Steve Hommy
+# Description: Pseudocode where user inputs the exercise points and program prints out the grade.
+
+
+# User inputs points
+points = int(input("Insert maximum points: "))
+# Printing out grades 
+if points < 60:
+    print("Grade 0")
+elif points == 60:
+    print("Grade 1")
+elif points <= 72:
+    print("Grade 2")
+elif points <= 84:
+    print("Grade 3")
+elif points <= 96:
+    print("Grade 4")
+else:
+    print("Grade 5")
