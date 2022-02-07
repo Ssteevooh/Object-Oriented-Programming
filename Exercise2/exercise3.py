fe23060908e4929e0d198491f78ceb0ecd2fb7d7
Steve Hommy <steve96hommy@gmail.com>
2022-02-07 12:09:14 +0200

    Exercise2_3 program prints out the grade

--- Exercise2/exercise3.py
+++ Exercise2/exercise3.py
@@ -3,18 +3,27 @@
 # Description: Pseudocode where user inputs the exercise points and program prints out the grade
 
 
-# User inputs points
-points = int(input("Insert maximum points: "))
-# Printing out grade 
-if points < 60:
-    print("Grade 0")
-elif points == 60:
-    print("Grade 1")
-elif points <= 72:
-    print("Grade 2")
-elif points <= 84:
-    print("Grade 3")
-elif points <= 96:
-    print("Grade 4")
-else:
-    print("Grade 5")
+def grade_calculator():
+    while True:
+        try:
+            points = int(input("Insert exercise points: "))
+            if points < 0 or points > 120:
+                raise ValueError
+            break
+        except ValueError:
+            print("Error: exercise points cannot be < 0 or > 120.")
+    if points >= 108:
+        print("Grade 5")
+    elif points >= 96:
+        print("Grade 4")
+    elif points >= 84:
+        print("Grade 3")
+    elif points >= 72:
+        print("Grade 2")
+    elif points >= 60:
+        print("Grade 1")
+    else:
+        print("Grade 0")
+
+grade_calculator()
+
