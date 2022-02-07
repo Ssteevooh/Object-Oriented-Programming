baf80d6cbc40c28108aadcb7b310d956cef9d809
Steve Hommy <steve96hommy@gmail.com>
2022-02-07 12:08:15 +0200

    Exercise1_9 returns a random number 1-6

--- Exercise1/exercise9.py
+++ Exercise1/exercise9.py
@@ -7,10 +7,9 @@ import random
 
 
 # Defining function
-def random_number(x, y):
-    # random.randint return a random integer between x = 1 y = 6
-    print("Random number is: " + str(random.randint(x, y)))
-
+def random_number():
+    # random.randint return a random integer between 1 to 6
+    return random.randint(1, 6)
 
 # Calling the function
-random_number(1, 6)
+print(random_number())
