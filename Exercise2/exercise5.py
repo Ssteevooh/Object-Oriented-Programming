95228103c6fb3c843350a9498cd9c4c4329ab2d8
Steve Hommy <steve96hommy@gmail.com>
2022-02-07 12:10:25 +0200

    Exercise2_5 Calculate average grade

--- Exercise2/exercise5.py
+++ Exercise2/exercise5.py
@@ -1,15 +1,39 @@
 # File name: exercise5.py
 # Author: Steve Hommy
-# Description: 
+# Description: Calculate average grade
 
 
-name_list = []
-points_list = []
-students_in_course = int(input("How many students are in this course? "))
+def students():
+    student_list = []
+    grade_list = []
+    while True:
+        # The lower() method returns a string where all characters are lower case
+        # the strip() method remove spaces at the beginning and at the end of the string
+        add_student = str(input("Want to input more students (Y/N): ")).lower().strip()
+        try:
+            if add_student[0] == "y":
+                student_name = input("Give name: ")
+                student_grade = int(input("Give grade: "))
+                try:
+                    if student_grade < 0 or student_grade > 5:
+                        raise ValueError
+                    student_list.append(student_name)
+                    grade_list.append(student_grade)
+                except ValueError:
+                    print("Error: grade cannot be < 0 or > 5.")
+            elif add_student[0] == "n":
+                average = sum(grade_list) / len(student_list)
+                # the zip() brings elements of the same index from multiple iterable 
+                # objects together as elements of the same tuples
+                for name, grade in zip(student_list, grade_list):
+                    print(name + " grade is", grade)
+                    # Formating average grade into 2 decimal
+                print("Average grade of students is: " + " {:.2f}".format(average))
+                break
+            else:
+                print("Invalid input")
+        except Exception as error:
+            print("Please input Y or N: ")
+            print(error)
 
-for i in range(students_in_course):
-    student_name = input("Name of the student: ")
-    name_list.append(student_name)
-    student_points = int(input("Points of the student: "))
-    points_list.append(student_points)
-
+students()
