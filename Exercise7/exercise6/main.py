98dfffd2d64af532b054efe4ad59e12260361079
Steve Hommy <steve96hommy@gmail.com>
2022-03-22 01:01:40 +0200

    Exercise7

--- Exercise7/exercise6/main.py
+++ Exercise7/exercise6/main.py
@@ -3,21 +3,50 @@
 # Description: Main function
 
 import random
-import os
 
-filename = "capitals.txt"
-countrys = {}
+filename = "Exercise7/exercise6/capitals.txt"
+dictionary = {}
 
 try:
-    with open(filename) as f:
-        for line in f:
-            (key, capital) = line.split(" : ")
-            (val, space) = capital.split("\n")
-            countrys[key] = val
+    with open(filename) as file:
+        for line in file:
+            (key, value) = line.split()
+            dictionary[key] = value
 except FileNotFoundError:
     msg = "Sorry, the file " + filename + " does not exist.\n"
     print(msg)
 
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-print("Files in %r: %s" % (cwd, files))
+shuffle = list(dictionary.items())
+random.shuffle(shuffle)
+dictionary = dict(shuffle)
+country_lst = list(dictionary)
+
+
+print("The Capital quiz")
+print("Write the capital of the country")
+print()
+
+while True:
+
+    points = 0
+
+    # With country lists index value checks if user input matches dictionarys
+    # key value. Correct awnser gives user a point and íncorret awnser returns correct
+    # awnser.
+
+    for i in range(10):
+
+        print(country_lst[i])
+
+        user_input = input(": ")
+
+        if user_input == dictionary[country_lst[i]]:
+            print("Correct!")
+            print()
+            points += 1
+
+        else:
+            print("Wrong, correct awnser is", dictionary[country_lst[i]])
+            print()
+
+    print("Your score was", str(points) + "/10")
