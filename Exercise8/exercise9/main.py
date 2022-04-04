3ce2f783f86e84b30a42d22b6a38635514288d31
Steve Hommy <steve96hommy@gmail.com>
2022-04-05 01:13:24 +0300

    Exercise9

--- Exercise8/exercise9/main.py
+++ Exercise8/exercise9/main.py
@@ -2,5 +2,21 @@
 # Author: Steve Hommy
 # Description: Main function
 
-def zeroDiv():
-    
+def zeroDiv(x, y):
+    try:
+        return x/y
+    except ZeroDivisionError:
+        return 0
+
+def valEr(var):
+    try:
+        return int(var)
+    except ValueError:
+        print("The argument does not contain numbers")
+
+def naEr(name):
+    try:
+        print(name)
+    except NameError:
+        print("Variable is not defined")
+
