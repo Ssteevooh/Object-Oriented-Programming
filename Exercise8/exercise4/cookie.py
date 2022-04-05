3e2e837218e3e372b118a0d42045c97b61a9555a
Steve Hommy <steve96hommy@gmail.com>
2022-04-05 09:40:06 +0300

    Exercise8

--- Exercise8/exercise4/cookie.py
+++ Exercise8/exercise4/cookie.py
@@ -4,24 +4,25 @@
 
 
 class Cookie:
-    def __init__(self, bake, frost):
-        self.bake = bake
-        self.frost = frost
+    def __init__(self):
+        self.bake = False
+        self.frost = None
+        self.flavor = ["Vanilla", "Chocolate", "Toffe"]
 
     def __str__(self):
         return f"""
         Cookie is baked: {self.bake}
-        Cookie is frosted: {self.frost}"""
+        Cookie is frosted: {self.frost}
+        """
 
     def set_bake(self):
-        self.bake = False
+        self.bake = True
 
     def get_bake(self):
         return self.bake
 
-    def set_frost(self):
-        self.frost = False
+    def set_frost(self, choice):
+        self.frost = self.flavor[choice]
 
     def get_frost(self):
         return self.frost
-
--- Exercise8/exercise4/main.py
+++ Exercise8/exercise4/main.py
@@ -1,3 +1,57 @@
 # File: main.py
 # Author: Steve Hommy
 # Description: Main function
+
+
+from cookie import Cookie
+import random
+
+
+while True:
+    flavor_list = ["Vanilla", "Chocolate", "Toffe"]
+    chosen_flavor = input("Choose flavor Vanilla, Chocolate, Toffe: ")
+    if chosen_flavor in flavor_list:
+        break
+
+
+def main(chosen_flavor):
+    choice = random.randint(0, 2)
+    cookie_list = []
+
+    for object in range(10):
+        object = Cookie()
+        cookie_list.append(object)
+
+    # Bakes every cookie in the list one by one
+    baked = 0
+    for object in cookie_list:
+        object.set_bake()
+        baked += 1
+        if baked == len(cookie_list):
+            print("All cookies baked")
+            print()
+
+    # Frosts every cookie in the list one by one
+    frosted = 0
+    for object in cookie_list:
+        object.set_frost(choice)
+        frosted += 1
+        if frosted == len(cookie_list):
+            print("All cookies frosted")
+            print()
+
+    # Checks if the flavor is the same
+    # If it's wrong do it all again
+    test_cookie = cookie_list[0]
+    if test_cookie.get_frost() != chosen_flavor:
+        print("Flavor is wrong, everything has to be done again")
+        main(chosen_flavor)
+
+    # If flavor is right
+    else:
+        print("All cookies are correctly done")
+        for object in cookie_list:
+            print(object)
+
+
+main(chosen_flavor)
